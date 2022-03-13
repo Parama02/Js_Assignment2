a10e70f94ac3ad23bb3c53c4a4fe7c483107b3ed
Parama02 <99189639+Parama02@users.noreply.github.com>
2022-03-13 20:30:42 +0530

    edit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var strName="Skill Safari"
+
+// To find length
+
+console.log("Lenght of the string: "+strName.length)
+
+// to make all capital for strName
+
+console.log("String converted to all letters to capital: "+strName.toUpperCase())
+
+// index position of "a"
+
+console.log("Index of letter \"a\": "+strName.indexOf("a"))
+
+// Conversation
+
+console.log("Captain America:\nBig man in a suit of armor\ntake that off what you are ?\nIron Man:\nGenius, Billionaire, Play boy, Philanthropist\nCaptain America:\nI know guys worth none of that worth 10 of you\nYou know, you may not be a threat\nbut you better stop pretending to be a Hero\nIron man:\nA hero ?\nlike you ?\nYou\'re a laboratory experiment Rogers\nEverything special about you came out of a bottle.")
+
+// Body Mass Index Logic h->height in cm w->weight in kg 
+// Formula for BMI= (w/((h*h)/100))*100
+
+console.log("\nBMI example 1\n")
+//BMI calculation1
+var h=200
+var w=80
+var BMI= (w/((h*h)/100))*100
+
+console.log("BMI: "+BMI+" kilogram/meter square")
+console.log("\nBMI example 2\n")
+//BMI calculation2
+var h=200
+var w=60
+var BMI= (w/((h*h)/100))*100
+
+console.log("BMI: "+BMI+" kilogram/meter square")
